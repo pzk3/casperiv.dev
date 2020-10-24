5ba8320ab7fd95a25512b0e3858d8523cc2fc0fc
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-10-24 20:37:04 +0200

    :sparkles: added icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -28,6 +28,40 @@ class MyDocument extends Document {
             defer
             src="https://cdnjs.cloudflare.com/ajax/libs/zenscroll/4.0.2/zenscroll-min.js"
           ></script>
+          <link
+            rel="shortcut icon"
+            href="/icons/icon-128.png"
+            type="image/x-icon"
+          />
+          <link
+            sizes="128x128"
+            rel="apple-touch-icon"
+            href="/icons/icon-128.png"
+          />
+          <link
+            sizes="144x144"
+            rel="apple-touch-icon"
+            href="/icons/icon-144.png"
+          />
+          <link
+            sizes="250x250"
+            rel="apple-touch-icon"
+            href="/icons/icon-250.png"
+          />
+          <link
+            sizes="512x512"
+            rel="apple-touch-icon"
+            href="/icons/icon-512.png"
+          />
+          <link
+            sizes="1080x1080"
+            rel="apple-touch-icon"
+            href="/icons/icon-1080.png"
+          />
+          <meta
+            property="og:image"
+            content="https://caspertheghost.me/assets/img/icons/icon-128.png"
+          />
         </Head>
         <body>
           <Main />
