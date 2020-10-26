b0577961327cc4edb37e251f41a097d27f5bbcc0
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-10-26 19:14:58 +0100

    added footer

---
+++ components/Footer.tsx
@@ -0,0 +1,17 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <img className="footer__icon" src="/icons/icon-128.png" alt="footer" />
+
+      <p>
+        Made by{" "}
+        <a href="https://github.com/dev-caspertheghost/caspertheghost.me">
+          CasperTheGhost
+        </a>{" "}
+        in 2020 <span>❤️</span>
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
