a10b0fb6eb2ec8d316c875ec33dd4ab7feb9f3c9
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2020-11-04 14:12:26 +0100

    :tada: added better favicons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,22 +16,38 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Catamaran:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
           />
-
           <script
             defer
             src="https://cdnjs.cloudflare.com/ajax/libs/zenscroll/4.0.2/zenscroll-min.js"
           ></script>
           <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/favicons/apple-touch-icon.png" />
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicons/favicon-32x32.png" />
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicons/favicon-16x16.png" />
+          <link rel="mask-icon" href="/favicons//safari-pinned-tab.svg" color="#2a2e33" />
+          <meta name="msapplication-TileColor" content="#2a2e33" />
           <meta name="theme-color" content="#2a2e33" />
-          <link rel="shortcut icon" href="/icons/icon-128.png" type="image/x-icon" />
-          <link sizes="128x128" rel="apple-touch-icon" href="/icons/icon-128.png" />
-          <link sizes="144x144" rel="apple-touch-icon" href="/icons/icon-144.png" />
-          <link sizes="250x250" rel="apple-touch-icon" href="/icons/icon-250.png" />
-          <link sizes="512x512" rel="apple-touch-icon" href="/icons/icon-512.png" />
-          <link sizes="1080x1080" rel="apple-touch-icon" href="/icons/icon-1080.png" />
+          <meta property="og:color" content="#2a2e33" />
+          <meta property="og:type" content="website" />
+          <meta property="og:url" content="https://caspertheghost.me" />
+          <meta property="og:title" content="Casper Iversen" />
+          <meta name="twitter:card" content="summary" />
+          <meta name="twitter:creator" content="@casper124578" />
+          <meta name="twitter:description" content="Hi I'm Casper Iversen! A web developer." />
+          <meta name="twitter:title" content="Casper Iversen" />
           <meta
             property="og:image"
             content="https://caspertheghost.me/assets/img/icons/icon-128.png"
+          />
+          <meta property="og:description" content="Hi I'm Casper Iversen! A web developer" />
+          <meta property="og:site_name" content="Casper Iversen" />
+          <meta
+            property="og:image"
+            content="https://caspertheghost.me/assets/img/icons/icon-128.png"
+          />
+          <meta
+            name="keywords"
+            content="caspertheghost, dev-caspertheghost, casper, @casper124578, Dev-CasperTheGhost, CasperTheGhost Dev, casper, iversen, casper iversen, @dev-caspertheghost, frontend dev, developer, frontend developer portfolio, portfolio, casper iversen portfolio, caspertheghost portfolio, caspertheghost github"
           />
         </Head>
         <body>
