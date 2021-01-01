10c6d192310861b8604d294e878baa54e6284f15
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-01-01 10:24:44 +0100

    :tada: updated timeline

--- data/timeline.ts
+++ data/timeline.ts
@@ -2,31 +2,38 @@ import TimelineItem from "../interfaces/Timeline";
 
 const timeline: TimelineItem[] = [
   {
+    date: "January 2021",
+    title: "Zyndicate",
+    text: "Joined the Zyndicate team 🎉!",
+    side: "left",
+    url: "https://zyndicate.app",
+  },
+  {
     date: "November 2020",
     title: "GitHub Bio",
     text: "Created a simple application to update my GitHub bio with my WakaTime stats",
-    side: "left",
+    side: "right",
     url: "https://github.com/Dev-CasperTheGhost/update-github-bio",
   },
   {
     date: "October 2020",
     title: "Portfoliov3",
     text: "Rewritten and redesigned my portfolio in NextJs and TypeScript",
-    side: "right",
+    side: "left",
     url: "https://github.com/dev-caspertheghost/caspertheghost.me",
   },
   {
     date: "October 2020",
     title: "SnailyCADv3",
     text: "Rewriting my first project in TypeScript, making it more secure and better.",
-    side: "left",
+    side: "right",
     url: "https://github.com/dev-caspertheghost/snaily-cadv3",
   },
   {
     date: "September 2020",
     title: "TypeScript",
     text: "Started learning TypeScript. I'm liking it and It's going great so far!",
-    side: "right",
+    side: "left",
     url: "https://www.typescriptlang.org/",
   },
   {
@@ -34,7 +41,7 @@ const timeline: TimelineItem[] = [
     title: "Notey.app",
     text: "Working on another big project! Notes app to keep track of important things",
     url: "https://notey.caspertheghost.me",
-    side: "left",
+    side: "right",
   },
   {
     date: "July 2020",
@@ -42,47 +49,47 @@ const timeline: TimelineItem[] = [
     text:
       "Recreated my old Discord bot and made it even better, learned a lot from my old mistakes.",
     url: "https://github.com/dev-caspertheghost/ghostybot",
-    side: "right",
+    side: "left",
   },
   {
     date: "July 2020",
     title: "React Timeline",
     text: "Created my first NPM package! Create cool and good looking timelines with ease.",
     url: "https://www.npmjs.com/package/react-timelinev2",
-    side: "left",
+    side: "right",
   },
   {
     date: "July 2020",
     title: "FreeCodeCamp",
     text: "Started to learn more about data structures and algorithms on, Fun but hard!",
     url: "http://freecodecamp.com/",
-    side: "right",
+    side: "left",
   },
   {
     date: "June 2020",
     title: "Sololearn",
     text: "Started learning the basics of JavaScript again. Was very helpful.",
     url: "https://www.sololearn.com/Certificate/1024-18951663/pdf/",
-    side: "left",
+    side: "right",
   },
   {
     date: "March 2020",
     title: "Quarantine",
     text:
       "Quarantine was pretty cool, had a lot of time to learn more about ReactJs and could work on a lot of projects!",
-    side: "right",
+    side: "left",
   },
   {
     date: "End 2019 - Present",
     title: "SnailyCAD",
     text: "At the end of 2019 I started my first big project! SnailyCAD.",
-    side: "left",
+    side: "right",
   },
   {
     date: "2018 - 2019",
     title: "Beginning!",
     text: "2018, '19 were the years I fully got into programming and computers",
-    side: "right",
+    side: "left",
   },
 ];
 
