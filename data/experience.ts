11983385c40755fc01b75acc4a29755b58386b8e
Dev-CasperTheGhost <casper.iversen2@icloud.com>
2021-01-24 12:47:11 +0100

    :hammer: chore: add months to experience data

--- data/experience.ts
+++ data/experience.ts
@@ -3,26 +3,26 @@ import Experience from "../interfaces/Experience";
 const experience: Experience[] = [
   {
     title: "Zyndicate",
-    year: "2021 - Present",
+    year: "01/2021 - Present",
     description: "Working mostly on the frontend, currently making the settings pages.",
     stack: ["react.js", "next.js", "graphql", "typescript"],
   },
   {
     title: "Ghostybot",
-    year: "2019 - Present",
+    year: "07/2019 - Present",
     description:
       "A Discord bot for communities for helping them with automating things such as welcoming people, giving roles, playing music and more.",
     stack: ["mongodb", "javascript"],
   },
   {
     title: "Notey.app",
-    year: "2020 - Present",
+    year: "08/2020 - Present",
     description: "An awesome notes application that supports markdown.",
     stack: ["react.js", "redux", "express", "mongodb", "typescript", "javascript"],
   },
   {
     title: "SnailyCAD",
-    year: "2019 - Present",
+    year: "02/2019 - Present",
     description:
       "A project I created to give Gaming communities a free CAD/MDT to help with management of Police, EMS and dispatch roleplay.",
     stack: ["react.js", "redux", "mysql", "express", "typescript"],
