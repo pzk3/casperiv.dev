a667900e0d7034ae0bb1ccd83709128d9174fc83
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-03-04 12:53:41 +0100

    :hammer: chore: bump Preact

--- next.config.js
+++ next.config.js
@@ -53,6 +53,7 @@ module.exports = {
     if (!dev && !isServer) {
       config.resolve.alias = {
         ...config.resolve.alias,
+        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
         react: "preact/compat",
         "react-dom/test-utils": "preact/test-utils",
         "react-dom": "preact/compat",
