1ed0e8d32440f18091479289a75fadd66a51d5ea
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-05-25 15:00:06 +0200

    :tada: feat: new blog post

--- next.config.js
+++ next.config.js
@@ -49,17 +49,17 @@ module.exports = {
       },
     ];
   },
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.resolve.alias = {
-        ...config.resolve.alias,
-        "react/jsx-runtime.js": "preact/compat/jsx-runtime",
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      };
-    }
+  // webpack: (config, { dev, isServer }) => {
+  //   if (!dev && !isServer) {
+  //     config.resolve.alias = {
+  //       ...config.resolve.alias,
+  //       "react/jsx-runtime.js": "preact/compat/jsx-runtime",
+  //       react: "preact/compat",
+  //       "react-dom/test-utils": "preact/test-utils",
+  //       "react-dom": "preact/compat",
+  //     };
+  //   }
 
-    return config;
-  },
+  //   return config;
+  // },
 };
