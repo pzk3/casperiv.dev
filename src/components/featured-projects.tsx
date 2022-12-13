a1d4be832a3c73f3d3f07fa7981c2be02d46a226
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2022-12-13 16:10:00 +0100

    :hammer: chore: fix build

--- src/components/featured-projects.tsx
+++ src/components/featured-projects.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import { Project } from "types/project";
 import { Link } from "./blog/markdown/link";
 
--- src/components/my-backpack-section.tsx
+++ src/components/my-backpack-section.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 export function MyBackpackSection({ myBackpack }: { myBackpack: [string, string[]][] }) {
   return (
     <section id="skills">
