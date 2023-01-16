fbbfe3ac0519cac06a37e0195f7144dc03f3ca5f
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-01-16 18:39:30 +0100

    fix: incorrect path for 'edit on GitHub'

--- src/components/blog/blog-footer.tsx
+++ src/components/blog/blog-footer.tsx
@@ -8,26 +8,29 @@ interface Props {
 }
 
 const types = {
-  "/blog/[slug]": "posts",
-  "/snippets/[slug]": "snippets",
-  "/case-study/[slug]": "case-studies",
+  "/blog/": "posts",
+  "/snippets/": "snippets",
+  "/case-study/": "case-studies",
 };
 
 export function BlogFooter({ post }: Props) {
   const pathname = usePathname();
-
-  // todo: pathname doesn't work here
-  const type = types[pathname as keyof typeof types];
+  const type = findTypeByPathname(pathname);
 
   const GITHUB_REPO_LINK =
     "https://github.com/Dev-CasperTheGhost/caspertheghost.me/edit/main/src/data";
-  const link = `${GITHUB_REPO_LINK}/${type}/${post._raw.flattenedPath}`;
+  const link = `${GITHUB_REPO_LINK}/${post._raw.flattenedPath}.mdx`;
 
-  return (
+  return type ? (
     <footer className="flex justify-end pt-12 pb-2 mb-5 border-b border-secondary">
-      <a className="hover:underline" href={link}>
+      <a target="_blank" rel="noreferrer noopener" className="hover:underline" href={link}>
         Edit On GitHub
       </a>
     </footer>
-  );
+  ) : null;
+}
+
+function findTypeByPathname(pathname: string | null) {
+  if (!pathname) return null;
+  return Object.entries(types).find(([key]) => pathname.startsWith(key))?.[1];
 }
