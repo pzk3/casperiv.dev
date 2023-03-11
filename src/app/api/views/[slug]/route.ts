a7013a553a55ff0258d2fbab5701be2a033ccaf4
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-03-11 09:00:48 +0100

    🆕 feat(blog): How to create an npm package with tsup.

--- src/app/api/views/[slug]/route.ts
+++ src/app/api/views/[slug]/route.ts
@@ -1,6 +1,8 @@
 import { redisClient } from "lib/redis";
 
 export async function POST(_request: Request, { params }: { params: { slug: string } }) {
+  if (process.env.NODE_ENV === "development") return;
+
   const slug = params.slug;
   const view = await redisClient.incr(slug);
 
