492a27a1fbcc7ab228a271338cfc35d97fba6131
Dev-CasperTheGhost <53900565+Dev-CasperTheGhost@users.noreply.github.com>
2023-04-23 08:49:00 +0200

    chore: update deps

--- src/app/api/views/[slug]/route.ts
+++ src/app/api/views/[slug]/route.ts
@@ -1,9 +1,13 @@
 import { redisClient } from "~/lib/redis";
 
 export const revalidate = 0;
+export const runtime = "experimental-edge";
 
 export async function POST(_request: Request, { params }: { params: { slug: string } }) {
-  if (process.env.NODE_ENV === "development") return;
+  if (process.env.NODE_ENV === "development") {
+    const count = (await redisClient.get<string>(params.slug)) ?? 0;
+    return new Response(count.toString());
+  }
 
   const slug = params.slug;
   const view = await redisClient.incr(slug);
